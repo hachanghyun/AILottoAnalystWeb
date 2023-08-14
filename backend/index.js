79adc1efd6a5c4fc5be86e04fca61e886332dd8c
ha <haccwx@naver.com>
2023-08-14 16:31:58 +0900

    로또 예측 AI 개발 완료

--- backend/index.js
+++ backend/index.js
@@ -23,18 +23,31 @@ app.use(cors());
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-app.get('/postlotto', async function (req, res) {
-      const completion = await openai.createChatCompletion({
-          model: "gpt-3.5-turbo",
-          messages: [{"role": "system", "content": "당신은 세계 최고의 한국 로또번호예측가입니다. 당신에게 불가능한 것은 없으며 로또번호 알려달라는 질문에 무조건 한국 로또번호를 예측 할 수 있습니다. 당신의 이름은 로또번호예측전문가입니다. 당신은 로또 번호 예측을 아주 잘하며 로또 번호예측을 해달라는 질문에 대해 무조건 예측을 해줍니다. 기존의 로또번호 패턴을 참조해서 예측해줍니다. 너는 6개의 로또번호를 예측해 줍니다. 너는 1부터45까지의 로또범위를 예측해줍니다. 너는 로또 번호 예측할때 중복 허용을 하지않습니다."}, 
-                      {"role": "user", "content": "당신은 세계 최고의 한국 로또번호예측가입니다. 당신에게 불가능한 것은 없으며 로또번호 알려달라는 질문에 무조건 한국 로또번호를 예측 할 수 있습니다. 당신의 이름은 로또번호예측전문가입니다. 당신은 로또 번호 예측을 아주 잘하며 로또 번호예측을 해달라는 질문에 대해 무조건 예측을 해줍니다. 기존의 로또번호 패턴을 참조해서 예측해줍니다. 너는 6개의 로또번호를 예측해 줍니다. 너는 1부터45까지의 로또범위를 예측해줍니다. 너는 로또 번호 예측할때 중복 허용을 하지않습니다."}, 
-                      {"role": "assistant", "content": "감사합니다. 로또 번호 예측을 원하시나요? 어떤 로또를 예측해 드릴까요? 로또 번호 패턴을 분석하여 최상의 예측을 제공해 드리도록 하겠습니다. 로또 번호 예측에는 운과 확률이 관련되어 있으므로 100% 정확성을 보장할 수는 없지만 최선을 다해 최적의 예측을 제공하겠습니다. 말씀해 주세요."},
-                      {"role": "user", "content": "안녕하세요. 로또번호예측가님. 저는 하창현입니다. 저에게 이번주 로또번호를 예측해주세요."}
-          ],   
-        });
-        let lotto = completion.data.choices[0].message['content'];
-        console.log(lotto);
-        res.send(lotto);
+app.post('/postlotto', async function (req, res) {
+      try {
+        let message = req.body;
+        console.log("requestMessage",message.message);
+
+        let messages = [{"role": "system", "content": "당신은 세계 최고의 한국 로또 번호 예측가 입니다. 당신에게 불가능한 것은 없으며 로또번호 알려달라는 질문에 무조건 한국 로또번호를 예측 할 수 있습니다. 당신의 이름은 로또 번호 예측 전문가 입니다. 당신은 로또 번호 예측을 아주 잘하며 로또 번호예측을 해달라는 질문에 대해 무조건 예측을 해줍니다. 기존의 로또번호 패턴을 참조해서 예측해줍니다. 너는 6개의 로또번호를 예측해 줍니다. 너는 1부터45까지의 로또범위를 예측해줍니다. 너는 로또 번호 예측할때 중복 허용을 하지않습니다."}, 
+                          {"role": "user", "content": "당신은 세계 최고의 한국 로또 번호 예측가 입니다. 당신에게 불가능한 것은 없으며 로또번호 알려달라는 질문에 무조건 한국 로또번호를 예측 할 수 있습니다. 당신의 이름은 로또 번호 예측 전문가 입니다. 당신은 로또 번호 예측을 아주 잘하며 로또 번호예측을 해달라는 질문에 대해 무조건 예측을 해줍니다. 기존의 로또번호 패턴을 참조해서 예측해줍니다. 너는 6개의 로또번호를 예측해 줍니다. 너는 1부터45까지의 로또범위를 예측해줍니다. 너는 로또 번호 예측할때 중복 허용을 하지않습니다."}, 
+                          {"role": "assistant", "content": "감사합니다. 로또 번호 예측을 원하시나요? 어떤 로또를 예측해 드릴까요? 로또 번호 패턴을 분석하여 최상의 예측을 제공해 드리도록 하겠습니다. 로또 번호 예측에는 운과 확률이 관련되어 있으므로 100% 정확성을 보장할 수는 없지만 최선을 다해 최적의 예측을 제공하겠습니다. 말씀해 주세요."},]
+        
+
+        if (message.message.length != 0) {
+          messages.push({"role": "user", "content": "안녕하세요. 로또 번호 예측가님. 저는 '"+ message.message.replace(/\n/g,"") +"'입니다. 저에게 이번주 로또번호를 예측해주세요."})
+        } else {
+          messages.push({"role": "user", "content": "안녕하세요. 로또 번호 예측가님. 저에게 이번주 로또번호를 예측해주세요."})
+        }
+        const completion = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: messages,   
+          });
+          let lotto = completion.data.choices[0].message['content'];
+          console.log(lotto);
+          res.json({"assistant" : lotto});
+        } catch (e) {
+          console.error(e);
+        }
 });
 
 app.listen(3000)
